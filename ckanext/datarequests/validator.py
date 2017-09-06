81082f29a553e09afcc163de4be8df41a1a35b2a
WNjihia <dorisn172@gmail.com>
2017-09-06 15:56:01 +0300

    [Feature] add validate vote function

--- ckanext/datarequests/validator.py
+++ ckanext/datarequests/validator.py
@@ -79,3 +79,11 @@ def validate_comment(context, request_data):
 
     if len(comment) > constants.COMMENT_MAX_LENGTH:
         raise tk.ValidationError({tk._('Comment'): [tk._('Comments must be a maximum of %d characters long') % constants.COMMENT_MAX_LENGTH]})
+
+
+def validate_vote(context, request_data):
+    # Check if data request exists
+    try:
+        tk.get_action(constants.DATAREQUEST_SHOW)(context, {'id': request_data['datarequest_id']})
+    except Exception:
+        raise tk.ValidationError({tk._('Data Request'): [tk._('Data Request not found')]})
